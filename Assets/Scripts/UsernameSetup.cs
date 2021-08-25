dd2087fd85b332b8875fa0be2341a8b3554fbe4f
shairyl <shairylsrivastava@gmail.com>
2021-08-25 21:50:52 +1200

    Username Improvement

--- Assets/Scripts/UsernameSetup.cs
+++ Assets/Scripts/UsernameSetup.cs
@@ -2,14 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class UsernameSetup : MonoBehaviour
+public class UsernameSetup : MonoBehaviourPunCallbacks
 {
     public TMPro.TMP_InputField username;
 
     public void Setup()
     {
-        PlayerPrefs.SetString("username", username.text);
-        
+        if (string.IsNullOrEmpty(username.text))
+        {
+            PlayerPrefs.SetString("username", "Guest-Player");
+        }
+        else
+        {
+            PlayerPrefs.SetString("username", username.text);
+        }
+
+        Player[] players = PhotonNetwork.PlayerList;
+
+        for (int i = 0; i < players.Length; i++)
+        {
+           Debug.Log(players[i].ToString());
+        }
+       
+        /// PhotonNetwork.NickName;
+
+
     }
 }
