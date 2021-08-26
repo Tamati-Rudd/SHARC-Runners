5fbfe36b54195625868f4542b02c6100801c7a49
shairyl <shairylsrivastava@gmail.com>
2021-08-26 16:54:38 +1200

    Fixed Special Ability functionality

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -10,7 +10,8 @@ public class PlayerManager : MonoBehaviourPunCallbacks
     PhotonView PV;
     int selectedCharacter = 0;
     public MeterScript meterScript;
-    public Text counter;
+    public Text Counter;
+    private Canvas canvas;
 
 
     private void Awake()
@@ -37,7 +38,13 @@ public class PlayerManager : MonoBehaviourPunCallbacks
 
     void CreateController()
     {
-        if(selectedCharacter == 0)
+        Vector2 meterlocation;
+        meterlocation.x = 50;
+        meterlocation.y = 50;
+
+        canvas = GameObject.FindGameObjectWithTag("Canvas").GetComponent<Canvas>();
+
+        if (selectedCharacter == 0)
         {
             //Spawn the Player
             PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerBlue"), Vector2.zero, Quaternion.identity);
@@ -47,8 +54,14 @@ public class PlayerManager : MonoBehaviourPunCallbacks
         {
             //Spawn the Player
             GameObject prefab = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerRed"), Vector2.zero, Quaternion.identity);
-            prefab.GetComponent<Collectable>().abilityMeter = meterScript;
-            prefab.GetComponent<Collectable>().Counter = counter;
+
+            MeterScript meter = Instantiate(meterScript, meterlocation, Quaternion.identity);
+            meter.transform.SetParent(canvas.transform);
+            prefab.GetComponent<Collectable>().abilityMeter = meter;
+
+            Text counterclone = Instantiate(Counter, meterlocation, Quaternion.identity);
+            counterclone.transform.SetParent(canvas.transform);            
+            prefab.GetComponent<Collectable>().Counter = counterclone;
         }
         if (selectedCharacter == 2)
         {
