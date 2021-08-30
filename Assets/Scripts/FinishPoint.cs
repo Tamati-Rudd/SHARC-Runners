0c4402735829c01d9b86280852d0f54683d3d0e7
vxz7784 <vxz7784@autuni.ac.nz>
2021-08-30 15:41:23 +1200

    Finish Line Winner Working
    Version of the finish line with the winner of the race displayed in the post game screen

--- Assets/Scripts/FinishPoint.cs
+++ Assets/Scripts/FinishPoint.cs
@@ -3,29 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Photon.Pun;
-using UnityEngine.UI;
-using TMPro;
+
 
 //NOTE: For this scene to function correctly, the next built scene after the game scene MUST be the game ended scene
 
 public class FinishPoint : MonoBehaviour 
 {
     PhotonView winnerPV;
-    //Canvas canvas;
-    //TextMeshPro winnerText;
+    public static string winnerName;
 
     void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "Player") { //If the colliding object has the Player tag
-            winnerPV = collision.GetComponent<PhotonView>(); //Get the winner
-
+            winnerPV = collision.GetComponent<PhotonView>(); 
+            winnerName = winnerPV.Owner.NickName; //Record the winner's name
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1); //Load the next built scene (will be the post game scene)
-            //Instansiate()
-            //GameObject canvas = GameObject.FindGameObjectWithTag("CanvasUI");
-            //canvas = tempObject.GetComponent<Canvas>(); //Get the canvas so the text objects can be accessed
-            //winnerText = GameObject.Find("WinnerText").GetComponent<TextMeshPro>();
-            //if (winnerText == null)
-                //Debug.Log("Problem: Winner Text Reference is Null");
-            //winnerText.text = winnerPV.Owner.NickName + "Wins!";
         }
     }
 }
