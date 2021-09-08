8c3e0614eace3b9bc3df087f6ec82d88ee9ed106
vxz7784 <vxz7784@autuni.ac.nz>
2021-09-08 15:55:58 +1200

    Sabotage is Selected
    The sabotage method of the SabotageController class is now called when a sabotage crate is collected to randomly select a sabotage. The unit test also passes

--- Assets/Scripts/Sabotagable.cs
+++ Assets/Scripts/Sabotagable.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
+//This class manages player collision with sabotage crates
 public class Sabotagable : MonoBehaviour
 {
-    public PhotonView PV; //The photon view of the crate
+    public PhotonView PV; 
 
     void Awake()
     {
@@ -18,11 +19,18 @@ public class Sabotagable : MonoBehaviour
         {
             Debug.Log("Sabotage pickup collected!");
 
-            int viewID = collision.GetComponent<PhotonView>().ViewID;
+            //Remove the sabotage crate
+            int viewID = collision.GetComponent<PhotonView>().ViewID; //Get crates viewID
             PV.RPC("DestroySabotageCrate", RpcTarget.AllBuffered, viewID);
+
+            //Select and apply the sabotage
+            SabotageController sabController = GameObject.FindGameObjectWithTag("SabotageController").GetComponent<SabotageController>();
+            PlayerController sourceController = GetComponent<PlayerController>();
+            sabController.sabotage(sourceController);
         }
     }
 
+    //Destroy a sabotage crate
     [PunRPC]
     public void DestroySabotageCrate(int viewID)
     {
