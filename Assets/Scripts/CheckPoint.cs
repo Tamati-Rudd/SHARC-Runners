1a2126eb46b072c5651f4252a1043604132642cd
vxz7784 <vxz7784@autuni.ac.nz>
2021-10-11 03:42:19 +1300

    Missile Sabotage & Other Improvements
    This large commit adds the 3rd and final sabotage type to the game - the missile trap sabotage (fires a homing missile at players). As of this commit, ALL THREE SABOTAGES ARE ACTIVE and can be encountered in a game.
    
    A variety of improvements are also made:
    - Blindness sabotage has been slightly tweaked to work on both maps
    - Various method renames to match project case standards (based on code review). Anything using these methods has been updated accordingly.
    - Finishing the race now moves a player to a spectate layer, which is tuned to ignore collisions with anything on the enemy layer
    - Sabotage scripts moved into a sabotage folder located inside the scripts folder for easier organization

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class CheckPoint : MonoBehaviour
 {
@@ -22,16 +23,24 @@ public class CheckPoint : MonoBehaviour
         }
     }
 
+    //This method handles player death, which occurs upon collision with enemies or missiles
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //Check if the target object is an enemy
-        if (collision.gameObject.tag == "Enemy")
-        {
+        if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Missile")
+        {    
             if (!(playerController.raceFinished)) //Move player back to the passed check point
                 playerController.PV.transform.position = checkPointRespawn;
             else //Move player back to last passed check point while spectating. Done to prevent disruption of the race by spectators
                 playerController.PV.transform.position = new Vector3(checkPointRespawn.x, checkPointRespawn.y, -100);
+
+            //If the collision was with a missile, verify the collision was with the missile's target and end the missile early
+            if (collision.gameObject.tag == "Missile")
+            {
+                SabotageMissile missile = collision.gameObject.GetComponent<SabotageMissile>();
+                PlayerController target = missile.target;
+                if (target.PV == playerController.PV)
+                    missile.EndMissile();
+            }
         }
     }
-
 }
